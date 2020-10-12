f77d4a830086822fe57eaeb7941de9803d15817b
Waheed Afolabi <waheed.freelancer16@gmail.com>
2020-10-12 01:01:55 +0100

    add reusable Starbucks view controller

---
+++ StarBucksClone/ViewControllers/ScanViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ScanViewController.swift
+//  StarBucksClone
+//
+//  Created by waheedCodes on 12/10/2020.
+//
+
+import UIKit
+
+class ScanViewController: StarBucksViewController {
+    override func viewDidLoad() {
+        view.backgroundColor = .systemIndigo
+        title = "Scan"
+    }
+    
+    override func commonInit() {
+        setTabBarImage(imageName: "qrscan", title: "Sacn")
+    }
+}
