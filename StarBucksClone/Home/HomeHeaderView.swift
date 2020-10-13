d6e1119f4d215f832cb5c751b2b69037b354c15b
Waheed Afolabi <waheed.freelancer16@gmail.com>
2020-10-13 10:42:44 +0100

    add home header view base class

---
+++ StarBucksClone/Home/HomeHeaderView.swift
@@ -0,0 +1,23 @@
+//
+//  HomeHeaderView.swift
+//  StarBucksClone
+//
+//  Created by waheedCodes on 13/10/2020.
+//
+
+import UIKit
+
+class HomeHeaderView: UIView {
+    
+    var greetingLabel = UILabel()
+    var inboxButton = UIButton()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
