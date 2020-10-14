b9e4764ca5106e55997dd303afdcf7da86c49028
Waheed Afolabi <waheed.freelancer16@gmail.com>
2020-10-14 17:19:58 +0100

    refactor tileVC to get initialised with data

--- StarBucksClone/Home/TileViewController.swift
+++ StarBucksClone/Home/TileViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 class TileViewController: UIViewController {
     let tileView = TileView()
     
+    init(title: String, subtitle: String, imageName: String) {
+        super.init(nibName: nil, bundle: nil)
+        
+        tileView.titleLabel.text = title
+        tileView.subtitleLabel.text = subtitle
+        tileView.imageView.image = UIImage(named: imageName)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         styleTileViewController()
