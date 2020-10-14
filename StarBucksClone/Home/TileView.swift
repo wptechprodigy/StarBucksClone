f843c1df8894c11b8467a06e9a0e9b759637eace
Waheed Afolabi <waheed.freelancer16@gmail.com>
2020-10-14 18:07:06 +0100

    add shadow to the bottom of each tile

--- StarBucksClone/Home/TileView.swift
+++ StarBucksClone/Home/TileView.swift
@@ -8,6 +8,10 @@
 import UIKit
 
 class TileView: UIView {
+    private var shadowLayer: CAShapeLayer!
+    private var cornerRadius: CGFloat = 6
+    private var fillColor: UIColor = .white
+    
     let imageView = UIImageView()
     let titleLabel = UILabel()
     let subtitleLabel = UILabel()
@@ -27,4 +31,23 @@ class TileView: UIView {
         return CGSize(width: 100, height: 300)
     }
     
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        addShadow()
+    }
+    
+    func addShadow() {
+        shadowLayer = CAShapeLayer()
+        
+        shadowLayer.path = UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius).cgPath
+        shadowLayer.fillColor = fillColor.cgColor
+        
+        shadowLayer.shadowColor = UIColor.black.cgColor
+        shadowLayer.shadowPath = shadowLayer.path
+        shadowLayer.shadowOffset = CGSize(width: 0.0, height: 1.0)
+        shadowLayer.shadowOpacity = 0.2
+        shadowLayer.shadowRadius = 1
+        
+        layer.insertSublayer(shadowLayer, at: 0)
+    }
 }
