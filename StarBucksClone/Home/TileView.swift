9a415a63d2f6639b545c39cf4939489645a2cfc4
Waheed Afolabi <waheed.freelancer16@gmail.com>
2020-10-14 16:09:31 +0100

    add title, subtitle and cat button properties

--- StarBucksClone/Home/TileView.swift
+++ StarBucksClone/Home/TileView.swift
@@ -8,5 +8,23 @@
 import UIKit
 
 class TileView: UIView {
+    let imageView = UIImageView()
+    let titleLabel = UILabel()
+    let subtitleLabel = UILabel()
+    let ctaButton = makeGreenButton(withText: "Order")
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        style()
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: 100, height: 300)
+    }
     
 }
