5a8f0235ab0537592a4f6c890acd6a06195a8d88
Waheed Afolabi <waheed.freelancer16@gmail.com>
2020-10-16 14:01:28 +0100

    add balance points view to rewards tile view

--- StarBucksClone/Home/Rewards/RewardTileView.swift
+++ StarBucksClone/Home/Rewards/RewardTileView.swift
@@ -9,7 +9,19 @@ import UIKit
 
 class RewardTileView: UIView {
     
+    let balanceView = BalanceView()
+    
     override var intrinsicContentSize: CGSize {
         return CGSize(width: 100, height: 300)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        style()
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
