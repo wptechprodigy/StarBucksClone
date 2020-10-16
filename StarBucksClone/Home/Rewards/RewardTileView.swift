10f2edcdf9c705bd72d285e085ab7d54a85dce62
Waheed Afolabi <waheed.freelancer16@gmail.com>
2020-10-16 12:29:14 +0100

    set an intrinsic basic size for the view to display in the Rewards VC

---
+++ StarBucksClone/Home/Rewards/RewardTileView.swift
@@ -0,0 +1,15 @@
+//
+//  RewardTileView.swift
+//  StarBucksClone
+//
+//  Created by waheedCodes on 16/10/2020.
+//
+
+import UIKit
+
+class RewardsTileView: UIView {
+    
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: 100, height: 300)
+    }
+}
